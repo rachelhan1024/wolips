d0b6bde93c2ba24e5cc44984d4a7db81634867d5
Rachel Han <rhan@uow.edu.au>
2015-05-07 12:14:42 +1000

    fixup incorrect characters

--- wolips/core/plugins/org.objectstyle.wolips.wodclipse.core/java/jp/aonir/fuzzyxml/FuzzyXMLDocType.java
+++ wolips/core/plugins/org.objectstyle.wolips.wodclipse.core/java/jp/aonir/fuzzyxml/FuzzyXMLDocType.java
@@ -1,11 +1,5 @@
 package jp.aonir.fuzzyxml;
 
-/**
- * DOCTYPE宣言を示すノード。DOCTYPE宣言の編集はサポートしません。
- * このノードは親を持たず、FuzzyXMLDocumentオブジェクトから直接取得します
- * （getParentメソッドは常にnullを返します）。
- * また、FuzzyXMLElementの子要素として追加することはできません。
- */
 public interface FuzzyXMLDocType extends FuzzyXMLNode {
 
 	public String getName();
